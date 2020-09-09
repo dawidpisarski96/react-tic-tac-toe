a381e996bdd41cc6ae54bb682f476116e73e77fc
dawid <dawidpisarski96@gmail.com>
2020-09-09 14:30:38 +0200

    Updates

---
+++ precache-manifest.214c2060633d17f6c0307af50ae131fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b883ad72857d2af3c41e966cc1776d3",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "54625b4bc58c3984ee57",
+    "url": "/react-tic-tac-toe/static/css/main.9b2dbf92.chunk.css"
+  },
+  {
+    "revision": "333515a4d31e42dba7dd",
+    "url": "/react-tic-tac-toe/static/js/2.d51d0453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.d51d0453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54625b4bc58c3984ee57",
+    "url": "/react-tic-tac-toe/static/js/main.75a2ce81.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  }
+]);
